2de191b544ce0a125b0cb66642fcf6659edce19f
marwenDevBannour <marwen.bannour.dev@gmail.com>
2022-12-14 12:04:22 +0100

    commi project files BlazorUploadFiles

---
+++ BlazorUploadFiles/Server/Controllers/DocumentsController.cs
@@ -0,0 +1,69 @@
+﻿using BlazorUploadFiles.Server.Data;
+using BlazorUploadFiles.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorUploadFiles.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentsController : ControllerBase
+    {
+        private readonly DBContext _context;
+        private IWebHostEnvironment _environment;
+        public DocumentsController(DBContext context,IWebHostEnvironment env) { 
+         _context = context;
+          _environment = env;
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<UploadResult>>> UploadFile(List<IFormFile> files)
+        {
+            List<UploadResult> uploadResults = new List<UploadResult>();
+
+            foreach (var file in files)
+            {
+                var uploadResult = new UploadResult();
+                string trustedFileNameForFileStorage;
+                var untrustedFileName = file.FileName;
+                uploadResult.FileName = untrustedFileName;
+                //var trustedFileNameForDisplay = WebUtility.HtmlEncode(untrustedFileName);
+
+                trustedFileNameForFileStorage = Path.GetRandomFileName();
+                var path = Path.Combine(_environment.ContentRootPath, "uploads", trustedFileNameForFileStorage);
+
+                await using FileStream fs = new(path, FileMode.Create);
+                await file.CopyToAsync(fs);
+
+                uploadResult.StoredFileName = trustedFileNameForFileStorage;
+                uploadResult.ContentType = file.ContentType;
+                uploadResults.Add(uploadResult);
+
+                _context.Uploads.Add(uploadResult);
+            }
+
+            await _context.SaveChangesAsync();
+
+            return Ok(uploadResults);
+        }
+
+        [HttpGet("{fileName}")]
+        public async Task<IActionResult> DownloadFile(string fileName)
+        {
+            var uploadResult = await _context.Uploads.FirstOrDefaultAsync(u => u.StoredFileName.Equals(fileName));
+
+            var path = Path.Combine(_environment.ContentRootPath, "uploads", fileName);
+
+            var memory = new MemoryStream();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                await stream.CopyToAsync(memory);
+            }
+            memory.Position = 0;
+            return File(memory, uploadResult.ContentType, Path.GetFileName(path));
+        }
+
+    }
+}
---
+++ BlazorUploadFiles/Server/Data/DBContext.cs
@@ -0,0 +1,11 @@
+﻿using BlazorUploadFiles.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorUploadFiles.Server.Data
+{
+    public class DBContext:DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+        public DbSet<UploadResult> Uploads => Set<UploadResult>();
+    }
+}
